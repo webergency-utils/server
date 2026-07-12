427d0bf242d23c412f0f3b0af068b12c153b167c
Tomáš Korenko <radixxko@gmail.com>
2026-07-12 19:29:17 +0200

    chore: make update_imports regex generic and resolve Vitest decorator parsing issue

--- update_imports.js
+++ update_imports.js
@@ -3,14 +3,14 @@ import path from 'path';
 
 const filesToUpdate =
 [
-    { file : 'dist/compiler/transformer.js', relativePath : './ts.js', isStarImport : false },
-    { file : 'dist/compiler/transformer.d.ts', relativePath : './ts.js', isStarImport : false },
-    { file : 'dist/compiler/cli.js', relativePath : './ts.js', isStarImport : true },
-    { file : 'dist/compiler/swagger.js', relativePath : './ts.js', isStarImport : true },
-    { file : 'dist/compiler/swagger.d.ts', relativePath : './ts.js', isStarImport : true }
+    { file : 'dist/compiler/transformer.js', relativePath : './ts.js' },
+    { file : 'dist/compiler/transformer.d.ts', relativePath : './ts.js' },
+    { file : 'dist/compiler/cli.js', relativePath : './ts.js' },
+    { file : 'dist/compiler/swagger.js', relativePath : './ts.js' },
+    { file : 'dist/compiler/swagger.d.ts', relativePath : './ts.js' }
 ];
 
-for( const { file, relativePath, isStarImport } of filesToUpdate )
+for( const { file, relativePath } of filesToUpdate )
 {
     const filePath = path.resolve( file );
 
@@ -18,14 +18,10 @@ for( const { file, relativePath, isStarImport } of filesToUpdate )
     {
         let content = fs.readFileSync( filePath, 'utf8' );
 
-        if( isStarImport )
-        {
-            content = content.replace( "import * as ts from 'typescript';", `import ts from '${relativePath}';` );
-        }
-        else
-        {
-            content = content.replace( "import ts from 'typescript';", `import ts from '${relativePath}';` );
-        }
+        content = content.replace(
+            /import\s+(?:(?:\*\s+as\s+)?(\w+))\s+from\s+['"]typescript['"];?/g,
+            ( match, name ) => `import ${name} from '${relativePath}';`
+        );
 
         fs.writeFileSync( filePath, content, 'utf8' );
     }
